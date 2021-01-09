0a92739b02cd6388e53b41fe320d7045bfd36608
jleilani <jxleilani@gmail.com>
2021-01-09 18:14:12 -0500

    returning each pilot per starship

--- src/components/Starship.js
+++ src/components/Starship.js
@@ -1,23 +1,39 @@
-import React, { useState } from 'react'
+import Axios from "axios";
+import React, { useState } from "react";
+
 
 function Starship({ item }) {
   const [hide, setHide] = useState(true);
 
   const handleViewPilots = () => {
     setHide(!hide);
+    getPilots();
+  };
+
+  const handleClose = () => {
+    setHide(!hide);
+  };
+
+  const getPilots = () => {
+      item.pilots.forEach((pilot) => {
+        Axios.get(pilot)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err));
+      });
   };
 
   return (
     <div className="ship">
-      <p>
-        {item.name}
-      </p>
+      <p>{item.name}</p>
       <button onClick={handleViewPilots}>View Pilots</button>
       <div className={hide ? "pilots hide" : "pilots"}>
         {item.pilots}
+        <button className="btn-close" onClick={handleClose}>
+          X
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Starship
+export default Starship;
